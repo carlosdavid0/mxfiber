9c3eca73f288f3d159f975af6b7e9b777a25490f
david <romao.carlosdavi@gmail.com>
2024-04-26 12:35:30 -0300

    fix: tag for adopt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,12 +27,12 @@ export default function RootLayout({
     <html lang="pt-br">
       <Head>
         <meta name="adopt-website-id" content="0a8a3e95-6723-4736-9515-479b6972bb36" />
+        <Script className="adopt-injector" src="https://tag.goadopt.io/injector.js?website_code=0a8a3e95-6723-4736-9515-479b6972bb36" />
       </Head>
       <body className={inter.className}>
         <EmpresaProvider>
           {children}
         </EmpresaProvider>
-        <Script className="adopt-injector" src="https://tag.goadopt.io/injector.js?website_code=0a8a3e95-6723-4736-9515-479b6972bb36" />
       </body>
     </html>
   );
